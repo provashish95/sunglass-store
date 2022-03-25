d8bf337d9eb70fc36737169703fc4bdf1e252cfa
Provashish Roy <provashishroy95@gmail.com>
2022-03-25 21:52:39 +0600

    create data in products.json file to public folderr

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('products.json')
+      .then(res => res.json())
+      .then(data => setProducts(data))
+  }, []);
+
+  console.log(products);
+  products.map(product => <img src={product.picture}></img>);
+
   return (
     <div className="App">
-
+      <h1>Sunglass Store</h1>
+      {
+        products.map(product => <img src={product.picture}></img>)
+      }
     </div>
   );
 }
