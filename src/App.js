3b60a28dff94257f5de9f9b96941d700e66c6224
Provashish Roy <provashishroy95@gmail.com>
2022-03-26 00:45:14 +0600

    added bootstrap cards

--- src/App.js
+++ src/App.js
@@ -15,22 +15,26 @@ function App() {
   }, []);
 
   console.log(products);
-  //products.map(product => <img src={product.picture}></img>);
 
   return (
     <div className="App">
       <Header></Header>
       <div className="container">
-        <div className="row mt-5 d-flex">
-          <div className="col-12 col-md-8 col-lg-8 bg-info order-md-1 order-2 ">
-            <div className="row g-3">
+        <div className="row mt-5 d-flex justify-content-end">
+          <div className="col-12 col-md-8  order-2 order-md-1">
+            <div className="row g-4">
               {
-                products.map(product => <Product product={product}></Product>)
+                products.map(product => <Product key={product.id} product={product}></Product>)
               }
             </div>
           </div>
-          <div className="col-12 col-md-4 col-lg-4 bg-success order-md-2 order-1">
-            <h1 >This is cart box</h1>
+          <div className="col-12 col-md-4  order-1 order-md-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Header/Product/Product.js
+++ src/components/Header/Product/Product.js
@@ -2,9 +2,18 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
+    const { id, name, picture, price } = props.product;
     return (
-        <div className="col-12 col-md-4 border">
-            <h1>this is card</h1>
+        <div className="col-12 col-md-4">
+            <div class="card" style={{ width: '15rem' }}>
+                <img src={picture} class="card-img-top" alt="img" />
+                <div class="card-body">
+                    <h5 class="card-title">{name}</h5>
+                    <p class="card-text">Price: $ {price}</p>
+                    <p>ID: {id}</p>
+                    <button className="btn btn-primary">Add to cart</button>
+                </div>
+            </div>
         </div>
     );
 };
