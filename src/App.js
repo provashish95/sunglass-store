ce129de3b5b07988d2a2416877a963f367a16d19
Provashish Roy <provashishroy95@gmail.com>
2022-03-26 01:23:00 +0600

    updated folders path

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import Header from './components/Header/Header';
-import Product from './components/Header/Product/Product';
+import Product from './components/Product/Product';
+
 
 function App() {
 
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,13 +6,13 @@ const Product = (props) => {
     const { id, name, picture, price } = props.product;
     return (
         <div className="col-12 col-md-4 d-flex justify-content-center">
-            <div className="card shadow-lg p-3 mb-3 bg-body rounded">
+            <div className="card shadow-lg p-3 mb-3 bg-body rounded text-center">
                 <img src={picture} className="card-img-top" alt="img" />
                 <div className="card-body">
                     <h5 className="card-title">{name}</h5>
                     <p className="card-text">Price: $ {price}</p>
                     <p>ID: {id}</p>
-                    <button className="btn btn-primary">Add to cart <BsFillCartCheckFill></BsFillCartCheckFill></button>
+                    <button className="btn  cart-btn">Add to Cart <BsFillCartCheckFill className="btn-icon"></BsFillCartCheckFill></button>
                 </div>
             </div>
         </div>
