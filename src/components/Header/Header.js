a199d1cdd3c6b543715e1bad66bf8302042e8a99
Provashish Roy <provashishroy95@gmail.com>
2022-03-26 13:36:05 +0600

    added header text

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,8 +7,9 @@ const Header = () => {
             <nav className="navbar navbar-expand-lg navbar-bg">
                 <div className="container-fluid">
                     <ul className="navbar-nav m-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <h1 className="text-white">Sunglass Store</h1>
+                        <li className="nav-item text-navbar text-white">
+                            <h1 >Sunglass Store</h1>
+                            <h4>Select Your best Four</h4>
                         </li>
                     </ul>
                 </div>
