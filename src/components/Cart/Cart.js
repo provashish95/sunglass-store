fa99149e44e01321db49c64fb61a71b2f21239b0
Provashish Roy <provashishroy95@gmail.com>
2022-03-26 23:39:29 +0600

    updated

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,7 +7,7 @@ const Cart = (props) => {
     return (
         <div className="d-flex justify-content-start my-3 align-items-center gx-3">
             <img src={item.picture} className="cart-img" alt="img" />
-            <h5 className="fw-bold">{item.name}</h5>
+            <h5>{item.name}</h5>
             <span onClick={() => deleteItem(item.id)} className="delete-btn"><RiDeleteBinFill className="delete-btn-icon"></RiDeleteBinFill></span>
         </div>
 
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,7 +3,7 @@ import './Product.css';
 import { BsFillCartCheckFill } from 'react-icons/bs';
 
 const Product = (props) => {
-    const { id, name, picture, price } = props.product;
+    const { name, picture, price } = props.product;
     const { handleAddToCart } = props;
 
     return (
@@ -11,7 +11,7 @@ const Product = (props) => {
             <div className="card shadow-lg p-3 mb-3 bg-body rounded text-center">
                 <img src={picture} className="card-img-top" alt="img" />
                 <div className="card-body">
-                    <h5 className="card-title">{name}</h5>
+                    <h5 className="card-title text-color">{name}</h5>
                     <p className="card-text">Price: $ {price}</p>
                     <button onClick={() => handleAddToCart(props.product)} className="btn cart-btn">Add to Cart <BsFillCartCheckFill className="btn-icon"></BsFillCartCheckFill></button>
                 </div>
